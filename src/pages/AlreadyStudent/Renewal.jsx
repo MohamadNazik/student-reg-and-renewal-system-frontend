617f545fe04ec6f2e695ff1a92a8d4ce9949afe6
lasanga890 <lasangad890@gmail.com>
2024-12-24 20:57:45 +0530

    "Added Header component to Renewal page"

--- src/pages/AlreadyStudent/Renewal.jsx
+++ src/pages/AlreadyStudent/Renewal.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./../../components/Header";
 
 function Renewal() {
-  return <></>;
+  return (
+    <>
+      <Header title="Renewal of Registration" />
+    </>
+  );
 }
 
 export default Renewal;
