96ed3a5b3baad54f00213ae548755b9c9538e1f9
maleesha <maleesharathnayake840@gmail.com>
2025-01-01 21:03:22 +0530

    removre header

--- src/pages/RegistrationProcess/A1Form_Part01.jsx
+++ src/pages/RegistrationProcess/A1Form_Part01.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "../../components/Header";
+
 import holder_signature from "../../assets/signature.png"
 
 function A1Form_Part01() {
 
   return (
     <>
-      <Header title="A1Form_Part01" />
+     
 
     </>
   );
