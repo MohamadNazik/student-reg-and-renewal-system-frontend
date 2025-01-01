4ae4356e16680987c90fa2ed04bacf97df006be6
maleesha <maleesharathnayake840@gmail.com>
2025-01-01 19:53:48 +0530

    add header to A1Form_Part01 page

--- src/pages/RegistrationProcess/A1Form_Part01.jsx
+++ src/pages/RegistrationProcess/A1Form_Part01.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../../components/Header";
+import holder_signature from "../../assets/signature.png"
 
 function A1Form_Part01() {
-  return <div>A1Form_Part01</div>;
+
+  return (
+    <>
+      <Header title="A1Form_Part01" />
+
+    </>
+  );
 }
 
 export default A1Form_Part01;
