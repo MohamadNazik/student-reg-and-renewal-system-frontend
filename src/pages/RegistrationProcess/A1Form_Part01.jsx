08f73787ad57fca9d4a60439b9ce8a7867e91453
maleesha <maleesharathnayake840@gmail.com>
2025-01-02 20:32:19 +0530

    Update A1Form_Part01.jsx

--- src/pages/RegistrationProcess/A1Form_Part01.jsx
+++ src/pages/RegistrationProcess/A1Form_Part01.jsx
@@ -10,100 +10,100 @@ function A1Form_Part01() {
         <div className=" flex text-left items-center  h-screenitems-center text-sm md:text-lg border border-black rounded-sm focus:outline-1 xl:ml-20 focus:outline-black  px-2 py-1 w-[200px] h-[25px] md:w-[300px] mb-4 md:mb-6 justify-center">
           A1 - Enrollment Form</div>
         <header className="mb-4 md:flex sm:block justify-between md:mb-5">
-            <img
-              src={uov_logo}
-              alt="University Logo"
-              className="w-[60px] md:w-[130px] md:h-[130px] mx-auto mb-2"
-            />
-            <div className="block justify-center text-center mt-0 md:mt-4">
+          <img
+            src={uov_logo}
+            alt="University Logo"
+            className="w-[60px] md:w-[130px] md:h-[130px] mx-auto mb-2"
+          />
+          <div className="block justify-center text-center mt-0 md:mt-4">
             <h1 className="text-xl sm:text-lg md:text-2xl xl:text-2xl font-semibold">UNIVERSITY OF VAVUNIYA</h1>
             <p className="text-sm sm:text-lg xl:text-2xl text-gray-600">Personal Data of Students</p>
             <p className="text-sm sm:text-lg xl:text-2xl text-gray-600">
               Student Admission for the Academic Year 2022/23
             </p></div>
-            <img
-              src={Y_Image}
-              alt="your Image"
-              className="w-[50px] sm:w-[120px] md:h-[130px] mx-auto mb-2 mt-2 md:mt-0 border border-black rounded-lg"
+          <img
+            src={Y_Image}
+            alt="your Image"
+            className="w-[50px] sm:w-[120px] md:h-[130px] mx-auto mb-2 mt-2 md:mt-0 border border-black rounded-lg"
+          />
+        </header>
+
+
+        <form
+          className="flex flex-col items-center gap-5">
+          {/* Enrollment Number */}
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
+              1. Enrollment No :-
+            </label>
+            <input
+              type="text"
+              name="enrollment_no"
+              className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
             />
-          </header>
+          </div>
 
+          {/* Name */}
 
-          <form
-            className="flex flex-col items-center gap-5">
-            {/* Enrollment Number */}
-            <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
-              <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
-                1. Enrollment No :-
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
+              2. Name :-
+            </label>
+          </div>
+
+          <div className="flex flex-col gap-2 sm:gap-4 ml-8">
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (i). Title :-
               </label>
-              <input
-                type="text"
-                name="Enrollment_No "
-                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[285px] sm:w-[455px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
-              />
-            </div>
-
-            {/* Name */}
-
-            <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
-              <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
-                2. Name :-
-              </label>
-            </div>
-
-            <div className="flex flex-col gap-2 sm:gap-4 ml-8">
-              <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
-                <label className="text-sm sm:text-lg xl:text-2xl font-medium">
-                  (i). Title :-
-                </label>
-                <div className="flex gap-2 sm:gap-7 ml-9">
+              <div className="flex gap-2 sm:gap-7 ml-9">
                 <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
-                    <input
-                      type="radio"
-                      name="state"
-                      value="Rev"
-                      className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
-                    />
-                    <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
-                      Rev.
-                    </span>
-                  </label>
-                  <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
-                    <input
-                      type="radio"
-                      name="state"
-                      value="Mr"
-                      className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
-                    />
-                    <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
-                      Mr.
-                    </span>
-                  </label>
-                  <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
-                    <input
-                      type="radio"
-                      name="state"
-                      value="Mrs"
-                      className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
-                    />
-                    <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
-                      Mrs.
-                    </span>
-                  </label>
-                  <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
-                    <input
-                      type="radio"
-                      name="state"
-                      value="Miss"
-                      className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
-                    />
-                    <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
-                      Miss.
-                    </span>
-                  </label>
-                </div>
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Rev"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Rev.
+                  </span>
+                </label>
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Mr"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Mr.
+                  </span>
+                </label>
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Mrs"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Mrs.
+                  </span>
+                </label>
+                <label className="flex items-center space-x-1 sm:space-x-2 xl:space-x-4">
+                  <input
+                    type="radio"
+                    name="state"
+                    value="Miss"
+                    className="w-3 h-3 sm:w-[14px] sm:h-[14px] xl:w-[16px] xl:h-[16px] rounded-full accent-[#391031]"
+                  />
+                  <span className="text-sm sm:text-lg xl:text-2xl font-semibold">
+                    Miss.
+                  </span>
+                </label>
               </div>
-              <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
               <label className="text-sm sm:text-sm xl:text-lg font-medium">
                 (other Please specify)
               </label>
@@ -112,96 +112,124 @@ function A1Form_Part01() {
                 name="title"
                 className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
               />
-              </div>
+            </div>
             <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
               <label className="text-sm sm:text-lg xl:text-2xl font-medium">
-                (ii). Name with Initials :
+                (ii). Name with Initials :-
               </label>
               <input
                 type="text"
                 name="name_with_initials"
-                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[100px] sm:w-[200px] xl:w-[300px] text-sm sm:text-lg xl:text-2xl py-1"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1"
               />
             </div>
             <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
               <label className="text-sm sm:text-lg xl:text-2xl font-medium">
-                (iii). Upload payment slip :
+                (iii). Name denoted by Initials :-
               </label>
               <input
-                type="file"
-                name="slip"
-                className="ml-3 sm:ml-0 border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[240px] sm:w-[350px] xl:w-[550px] text-sm sm:text-lg xl:text-2xl py-1"
+                type="text"
+                name="name_denoted_by_initials"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
               />
-              <span className="text-[12px] sm:text-[15px] xl:text-[18px] font-normal sm:font-medium text-gray-600 italic -mt-1 ml-4 sm:ml-[245px] xl:-mt-0 xl:ml-0">
-                jpeg, jpg, png, pdf
-              </span>
             </div>
-            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[265px] sm:w-[675px] xl:w-[1075px] mt-2">
-              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
-                (a) I under take to inform the Asst. Registrar of the Faculty
-                in writing of any change of address and in the event of
-                obtaining employment.
-              </label>
-            </div>
-            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[265px] sm:w-[675px] xl:w-[1075px] mt-2">
-              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
-                (b) I declare that the information furnished herein are true
-                and correct to the best of my knowledge.
-              </label>
-            </div>
-        </div>
 
-        {/* Address */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-700">
-            ADDRESS:
-          </label>
-          <textarea
-            placeholder="Permanent Address"
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-          <input
-            type="text"
-            placeholder="Province"
-            className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-          <input
-            type="text"
-            placeholder="District"
-            className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-          <input
-            type="text"
-            placeholder="Divisional Secretariat"
-            className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-          <input
-            type="text"
-            placeholder="National Identity Card No"
-            className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-          <input
-            type="tel"
-            placeholder="Mobile No"
-            className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-          <input
-            type="email"
-            placeholder="Email Address"
-            className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500"
-          />
-        </div>
+          </div>
+          {/* Address */}
+          <div className="flex flex-wrap gap-2 justify-between items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+            <label className="text-sm sm:text-lg xl:text-2xl font-medium uppercase">
+              2. Address :-
+            </label>
+          </div>
 
-        {/* Next Button */}
-        <button
-          type="button"
-          className="w-full bg-green-600 text-white py-2 px-4 rounded-lg shadow hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
-        >
-          NEXT
-        </button>
-      </form>
-    </div >
-      
+          <div className="flex flex-col gap-2 sm:gap-4 ml-8">
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[288px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (i). Permanent Address :-
+              </label>
+              <textarea
+                type="text"
+                name="permanent_address"
+                rows={3}
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[825px] text-sm sm:text-lg xl:text-2xl py-1 "
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (ii). province :-
+              </label>
+              <input
+                type="text"
+                name="province"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (ii). District :-
+              </label>
+              <input
+                type="text"
+                name="district"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (iii). Divisional Secretariat :-
+              </label>
+              <input
+                type="text"
+                name="divisional_secretariat"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (iv). National Identity Card No :-
+              </label>
+              <input
+                type="text"
+                name="divisional_secretariat"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (v). Mobile No :-
+              </label>
+              <input
+                type="text"
+                name="mobile_no"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+            <div className="flex flex-wrap gap-2 xl:gap-4 items-center w-[255px] sm:w-[673px] xl:w-[1115px]">
+              <label className="text-sm sm:text-lg xl:text-2xl font-medium">
+                (vi). Email Address :-
+              </label>
+              <input
+                type="text"
+                name="adddress"
+                className="border-2 border-black rounded-md focus:outline-1 focus:outline-black px-2 w-[220px] sm:w-[420px] xl:w-[650px] text-sm sm:text-lg xl:text-2xl py-1"
+              />
+            </div>
+
+          </div>
+
+
+          {/* Next Button */}
+          
+            <button
+              type="button"
+              className="md:ml-[1000px] w-[100px] bg-green-600 text-white py-2 px-4 rounded-lg shadow hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500"
+            >
+              NEXT
+            </button>
+          
+        </form>
+      </div >
+
 
     </>
   );
